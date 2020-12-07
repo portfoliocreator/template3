5aff7d70c97217f06548edff2525df4319157f4d
Aryan Gupta <aryanguptaleo@gmail.com>
2020-12-07 07:19:58 +0530

    add build

---
+++ precache-manifest.ab96fc07a57c5f3234d096410be33eca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1eac053e4a682d2cf3008f3cfe965b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a5045b4e5ed763da5a0",
+    "url": "/static/css/main.16802443.chunk.css"
+  },
+  {
+    "revision": "1bb81045fa33e6d8c50a",
+    "url": "/static/js/2.54f27e7c.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/static/js/2.54f27e7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a5045b4e5ed763da5a0",
+    "url": "/static/js/main.b5768ec7.chunk.js"
+  },
+  {
+    "revision": "fba0a7ae82e3114643a7",
+    "url": "/static/js/runtime-main.a0576299.js"
+  },
+  {
+    "revision": "fde3c0b1ed24a390fd6180981c7439c8",
+    "url": "/static/media/award.fde3c0b1.jpg"
+  },
+  {
+    "revision": "73e3ab59751b8dd4bf5c6c8be3335e21",
+    "url": "/static/media/language.73e3ab59.png"
+  },
+  {
+    "revision": "9e5663bc5a0db818f2eaf47a2567f092",
+    "url": "/static/media/publish.9e5663bc.jpg"
+  },
+  {
+    "revision": "756b31836ca3dfc446be2b22b4962be9",
+    "url": "/static/media/skill.756b3183.jpg"
+  }
+]);
